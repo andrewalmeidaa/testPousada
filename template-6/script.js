9a36e685b6ce831707bf10be7fc99e380edf42c0
Andrew Almeida <100097231+andrewalmeidaa@users.noreply.github.com>
2023-04-15 23:29:55 -0300

    create a new template

---
+++ template-6/script.js
@@ -0,0 +1,11 @@
+
+
+function openNav(){
+    document.getElementById('myNav').style.height = '100%'
+    // pra alterar se vai sair dos lados ou de cima e so mudar height para width
+}
+
+function closeNav(){
+    document.getElementById('myNav').style.height = '0'
+}
+
